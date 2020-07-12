bc28ca8c022f058c1c3046d26ee803829452aa20
Urosh <urosh_h@protonmail.com>
2020-07-12 14:36:52 +0200

    Form that connects the main form with skin preview form

---
+++ SkinProject/FConnector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkinProject
+{
+    public partial class FConnector : Form
+    {
+        public FConnector()
+        {
+            InitializeComponent();
+        }
+
+        public static bool result = false;
+
+        private void FConnector_Load(object sender, EventArgs e)
+        {
+            label1.Text = string.Format("{0} skins need repairing!", Form1.imagesToFix.Keys.Count);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FSkins f = new FSkins(); f.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
